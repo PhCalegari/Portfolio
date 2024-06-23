50b84681781ae2840f6fd385627741e3a0e4d883
Pedro H. Calegari <159385871+PhCalegari@users.noreply.github.com>
2024-06-23 19:27:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,99 @@
+function aumentarIcone(elemento) {
+    elemento.style.transform = 'scale(1.2)';
+}
 
-        function aumentarIcone(elemento) {
-            elemento.style.transform = 'scale(1.2)';
+function restaurarIcone(elemento) {
+    elemento.style.transform = 'scale(1)';
+}
+
+const ctxBar = document.getElementById('grafico-barra').getContext('2d');
+const ctxLine = document.getElementById('grafico-linha').getContext('2d');
+
+const graficoBarra = new Chart(ctxBar, {
+    type: 'bar',
+    data: {
+        labels: ['HTML', 'CSS', 'JavaScript', 'Python', 'SQL'],
+        datasets: [{
+            label: 'Habilidades',
+            data: [85, 80, 65, 70, 65],
+            backgroundColor: 'rgba(0, 255, 255, 0.2)',
+            borderColor: 'rgba(0, 255, 255, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
         }
+    }
+});
 
-        function restaurarIcone(elemento) {
-            elemento.style.transform = 'scale(1)';
+const graficoLinha = new Chart(ctxLine, {
+    type: 'line',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
+        datasets: [{
+            label: 'Progresso de Aprendizado',
+            data: [10, 12, 18, 40, 50, 60, 60],
+            backgroundColor: 'rgba(0, 255, 255, 0.2)',
+            borderColor: 'rgba(0, 255, 255, 1)',
+            borderWidth: 1,
+            fill: true
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
         }
-        var dadosGrafico1 = {
-            labels: ["HTML", "CSS", "JavaScript", "PHP"],
-            datasets: [{
-                label: "Nível de habilidade",
-                data: [80, 65, 45,58 , 0],
-                backgroundColor: 'rgba(0, 255, 255, 0.2)',
-                borderColor: 'rgba(0, 255, 255, 1)',
-                borderWidth: 1
-            }]
-        };
+    }
+});
 
-        let configGrafico = {
-            type: 'radar',
-            data: dadosGrafico1,
-            options: {
-                responsive: false,
-                maintainAspectRatio: true,
-                aspectRatio: 1,
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                },
-                scales: {
-                    r: {
-                        grid: {
-                            color: 'rgba(0, 255, 255, 0.2)'
-                        },
-                        ticks: {
-                            color: 'white'
-                        }
+const username = 'PhCalegari'; 
+const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao obter dados do GitHub');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const repoList = document.getElementById('repo-list');
+        data.forEach(repo => {
+            const repoContainer = document.createElement('div');
+            repoContainer.classList.add('repo-item');
+
+            const repoLink = document.createElement('a');
+            repoLink.href = repo.html_url;
+            repoLink.textContent = repo.name;
+            repoLink.target = '_blank';
+            repoLink.rel = 'noopener noreferrer';
+            repoLink.classList.add('retangulo');
+
+            repoContainer.appendChild(repoLink);
+            repoList.appendChild(repoContainer);
+        });
+        $('.repo-list').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1
                     }
                 }
-            }
-        };
-
-        let grafico1 = new Chart(
-            document.getElementById('grafico1'),
-            configGrafico
-        );
+            ]
+        });
+    })
+    .catch(error => {
+        console.error('Erro ao obter dados do GitHub:', error);
+    });
